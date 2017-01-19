e1ce464e10eaccb8b8df99e059e0b2afc0c48a42
Wulf2k <wulf2k@gmail.com>
2017-01-19 16:30:14 -0600

    Redid Read/Write functions

--- ParamEdit/frmParamEdit.vb
+++ ParamEdit/frmParamEdit.vb
@@ -7,9 +7,11 @@ Public Class frmParamEdit
     Shared Version As String
     Shared VersionCheckUrl As String = "http://wulf2k.ca/souls/ParamEdit-ver.txt"
 
-    Dim bytes() As Byte
     Dim paramDef() As paramDefs
     Dim bigEndian As Boolean = True
+
+    Dim fs As FileStream
+
 
     Structure paramDefs
         Public paramName As String
@@ -66,163 +68,201 @@ Public Class frmParamEdit
     End Sub
 
 
-    Private Sub bAdd(ByRef bArr1() As Byte, ByVal bArr2() As Byte)
-        Dim startIndex As Integer = bArr1.Length
 
-        Array.Resize(bArr1, bArr1.Length + bArr2.Length)
-        bArr2.CopyTo(bArr1, startIndex)
-    End Sub
+    Private Function RSingle(ByVal loc As UInteger) As Single
+        Dim tmpSingle As UInteger = 0
+        Dim byt = New Byte() {0, 0, 0, 0}
 
-    Private Function StrFromBytes(ByVal loc As UInteger) As String
-        Dim Str As String = ""
+        fs.Position = loc
+        fs.Read(byt, 0, 4)
+
+        If bigEndian Then
+            Array.Reverse(byt)
+        End If
+
+        tmpSingle = BitConverter.ToSingle(byt, 0)
+
+        Return tmpSingle
+    End Function
+    Private Function RInt8(ByVal loc As UInteger) As SByte
+        Dim tmpInt8 As Byte
+        Dim byt = New Byte() {0}
+
+        fs.Position = loc
+        fs.Read(byt, 0, 1)
+
+        tmpInt8 = byt(0)
+
+        Return tmpInt8
+    End Function
+    Private Function RInt16(ByVal loc As UInteger) As Int16
+        Dim tmpInt16 As UInteger = 0
+        Dim byt = New Byte() {0, 0}
+
+        fs.Position = loc
+        fs.Read(byt, 0, 2)
+
+        If bigEndian Then
+            Array.Reverse(byt)
+        End If
+
+        tmpInt16 = BitConverter.ToInt16(byt, 0)
+
+        Return tmpInt16
+    End Function
+    Private Function RInt32(ByVal loc As UInteger) As Int32
+        Dim tmpInt32 As UInteger = 0
+        Dim byt = New Byte() {0, 0, 0, 0}
+
+        fs.Position = loc
+        fs.Read(byt, 0, 4)
+
+        If bigEndian Then
+            Array.Reverse(byt)
+        End If
+
+        tmpInt32 = BitConverter.ToInt32(byt, 0)
+
+        Return tmpInt32
+    End Function
+    Private Function RUInt8(ByVal loc As UInteger) As Byte
+        Dim tmpUInt8 As Byte
+        Dim byt = New Byte() {0}
+
+        fs.Position = loc
+        fs.Read(byt, 0, 1)
+
+        tmpUInt8 = byt(0)
+
+        Return tmpUInt8
+    End Function
+    Private Function RUInt16(ByVal loc As UInteger) As UInt16
+        Dim tmpUInt16 As UInteger = 0
+        Dim byt = New Byte() {0, 0}
+
+        fs.Position = loc
+        fs.Read(byt, 0, 2)
+
+        If bigEndian Then
+            Array.Reverse(byt)
+        End If
+
+        tmpUInt16 = BitConverter.ToUInt16(byt, 0)
+
+        Return tmpUInt16
+    End Function
+    Private Function RUInt32(ByVal loc As UInteger) As UInt32
+        Dim tmpUInt32 As UInteger = 0
+        Dim byt = New Byte() {0, 0, 0, 0}
+
+        fs.Position = loc
+        fs.Read(byt, 0, 4)
+
+        If bigEndian Then
+            Array.Reverse(byt)
+        End If
+
+        tmpUInt32 = BitConverter.ToUInt32(byt, 0)
+
+        Return tmpUInt32
+    End Function
+    Private Function RAscStr(ByVal loc As UInteger) As String
+        Dim AscStr As String = ""
         Dim cont As Boolean = True
+        Dim len As Integer = 0
+        Dim byt As Byte
+
+        fs.Position = loc
 
         While cont
-            If bytes(loc) > 0 Then
-                Str = Str + Convert.ToChar(bytes(loc))
-                loc += 1
+            byt = fs.ReadByte
+
+            If byt > 0 Then
+                AscStr = AscStr & Convert.ToChar(byt)
             Else
                 cont = False
             End If
         End While
 
-        Return Str
-    End Function
-    Private Function bArrFromBytes(ByVal loc As UInteger) As Byte()
-        Dim bArray() As Byte = {}
-
-        Dim cont As Boolean = True
-        Dim i As Integer = 0
-
-        While cont
-            If loc + i < bytes.Length Then
-                If bytes(loc + i) > 0 Then
-                    ReDim Preserve bArray(bArray.Length)
-                    bArray(i) = bytes(loc + i)
-                    i += 1
-                Else
-                    cont = False
-                End If
-            Else
-                cont = False
-            End If
-        End While
-
-        Return bArray
+        Return AscStr
     End Function
 
-    Private Function Int16ToTwoByte(ByVal val As Integer) As Byte()
+    Private Sub WSingle(ByVal loc As UInteger, ByVal val As Single)
+        fs.Position = loc
+
+        Dim byt = BitConverter.GetBytes(val)
+
         If bigEndian Then
-            Return ReverseTwoBytes(BitConverter.GetBytes(Convert.ToInt16(val)))
-        Else
-            Return BitConverter.GetBytes(Convert.ToInt16(val))
+            Array.Reverse(byt)
         End If
-    End Function
-    Private Function Int32ToFourByte(ByVal val As Integer) As Byte()
-        If bigEndian Then
-            Return ReverseFourBytes(BitConverter.GetBytes(Convert.ToInt32(val)))
-        Else
-            Return BitConverter.GetBytes(Convert.ToInt32(val))
-        End If
-    End Function
-    Private Function UInt16TotwoByte(ByVal val As UInteger) As Byte()
-        If bigEndian Then
-            Return ReverseTwoBytes(BitConverter.GetBytes(Convert.ToUInt16(val)))
-        Else
-            Return BitConverter.GetBytes(Convert.ToUInt16(val))
-        End If
-    End Function
-    Private Function UInt32ToFourByte(ByVal val As UInteger) As Byte()
-        If bigEndian Then
-            Return ReverseFourBytes(BitConverter.GetBytes(Convert.ToUInt32(val)))
-        Else
-            Return BitConverter.GetBytes(Convert.ToUInt32(val))
-        End If
-    End Function
-    Private Function FloatToFourByte(ByVal val As String) As Byte()
-        If IsNumeric(val) Then
-            If bigEndian Then
-                Return ReverseFourBytes(BitConverter.GetBytes(Convert.ToSingle(val)))
-            Else
-                Return BitConverter.GetBytes(Convert.ToSingle(val))
-            End If
-        Else
-            Return {0, 0, 0, 0}
-        End If
-    End Function
-    Private Function ReverseFourBytes(ByVal byt() As Byte)
-        Return {byt(3), byt(2), byt(1), byt(0)}
-    End Function
-    Private Function ReverseTwoBytes(ByVal byt() As Byte)
-        Return {byt(1), byt(0)}
-    End Function
-    Private Sub InsBytes(ByVal loc As UInteger, ByVal byt As Byte())
-        For i = 0 To byt.Length - 1
-            bytes(loc + i) = byt(i)
-        Next
+
+        fs.Write(byt, 0, 4)
     End Sub
+    Private Sub WInt8(ByVal loc As UInteger, ByVal val As SByte)
+        fs.Position = loc
 
-    Private Function SingleFromFour(ByVal loc As UInteger) As Single
-        Dim bArray(3) As Byte
+        fs.WriteByte(val)
+    End Sub
+    Private Sub WInt16(ByVal loc As UInteger, ByVal val As Int16)
+        fs.Position = loc
 
-        For i = 0 To 3
-            bArray(3 - i) = bytes(loc + i)
-        Next
-        If Not bigEndian Then bArray = ReverseFourBytes(bArray)
-        Return BitConverter.ToSingle(bArray, 0)
-    End Function
-    Private Function SIntFromTwo(ByVal loc As UInteger) As Int16
-        Dim tmpint As Integer = 0
-        Dim bArray(1) As Byte
-
-        For i = 0 To 1
-            bArray(1 - i) = bytes(loc + i)
-        Next
-        If Not bigEndian Then bArray = ReverseTwoBytes(bArray)
-        tmpint = BitConverter.ToInt16(bArray, 0)
-        Return tmpint
-    End Function
-    Private Function SIntFromFour(ByVal loc As UInteger) As Integer
-        Dim tmpint As Integer = 0
-        Dim bArray(3) As Byte
-
-        For i = 0 To 3
-            bArray(3 - i) = bytes(loc + i)
-        Next
-        If Not bigEndian Then bArray = ReverseFourBytes(bArray)
-        tmpint = BitConverter.ToInt32(bArray, 0)
-        Return tmpint
-    End Function
-    Private Function UIntFromTwo(ByVal loc As UInteger) As UInteger
-        Dim tmpUint As UInteger = 0
+        Dim byt() As Byte
+        byt = BitConverter.GetBytes(val)
 
         If bigEndian Then
-            For i = 0 To 1
-                tmpUint += Convert.ToUInt16(bytes(loc + i)) * &H100 ^ (1 - i)
-            Next
-        Else
-            For i = 0 To 1
-                tmpUint += Convert.ToUInt16(bytes(loc + i)) * &H100 ^ (i)
-            Next
+            Array.Reverse(byt)
         End If
 
-        Return tmpUint
-    End Function
-    Private Function UIntFromFour(ByVal loc As UInteger) As UInteger
-        Dim tmpUint As UInteger = 0
+        fs.Write(byt, 0, 2)
+    End Sub
+    Private Sub WInt32(ByVal loc As UInteger, ByVal val As Int32)
+        fs.Position = loc
+
+        Dim byt() As Byte
+        byt = BitConverter.GetBytes(val)
 
         If bigEndian Then
-            For i = 0 To 3
-                tmpUint += Convert.ToUInt32(bytes(loc + i)) * &H100 ^ (3 - i)
-            Next
-        Else
-            For i = 0 To 3
-                tmpUint += Convert.ToUInt32(bytes(loc + i)) * &H100 ^ (i)
-            Next
+            Array.Reverse(byt)
         End If
 
-        Return tmpUint
-    End Function
+        fs.Write(byt, 0, 4)
+    End Sub
+    Private Sub WUInt8(ByVal loc As UInteger, ByVal val As Byte)
+        fs.Position = loc
+
+        fs.WriteByte(val)
+    End Sub
+    Private Sub WUInt16(ByVal loc As UInteger, ByVal val As UInt16)
+        fs.Position = loc
+
+        Dim byt() As Byte
+        byt = BitConverter.GetBytes(val)
+
+        If bigEndian Then
+            Array.Reverse(byt)
+        End If
+
+        fs.Write(byt, 0, 2)
+    End Sub
+    Private Sub WUInt32(ByVal loc As UInteger, ByVal val As UInt32)
+        fs.Position = loc
+
+        Dim byt() As Byte
+        byt = BitConverter.GetBytes(val)
+
+        If bigEndian Then
+            Array.Reverse(byt)
+        End If
+
+        fs.Write(byt, 0, 4)
+    End Sub
+    Private Sub WAscStr(ByVal loc As UInteger, ByVal str As String)
+        fs.Position = loc
+
+        Dim byt() As Byte
+        byt = System.Text.Encoding.ASCII.GetBytes(str)
+        fs.Write(byt, 0, byt.Length)
+    End Sub
 
 
     Private Sub txt_Drop(sender As Object, e As System.Windows.Forms.DragEventArgs) Handles txtParamdef.DragDrop, txtParam.DragDrop
@@ -237,7 +277,8 @@ Public Class frmParamEdit
         dgvParams.Rows.Clear()
         dgvParams.Columns.Clear()
 
-        bytes = File.ReadAllBytes(txtParamdef.Text)
+
+        fs = New IO.FileStream(txtParamdef.Text, IO.FileMode.Open)
 
         Dim length As UInteger
         Dim startOffset As UInteger
@@ -253,16 +294,16 @@ Public Class frmParamEdit
         Dim offset As UInteger
         Dim paramDefOffset As UInteger
 
-        length = UIntFromFour(&H0)
+        length = RUInt32(&H0)
 
-        If Not length = bytes.Length Then
+        If Not length = fs.Length Then
             bigEndian = False
-            length = UIntFromFour(&H0)
+            length = RUInt32(&H0)
         End If
 
-        startOffset = UIntFromTwo(&H4)
-        numEntries = UIntFromTwo(&H8)
-        entryLength = UIntFromTwo(&HA)
+        startOffset = RUInt16(&H4)
+        numEntries = RUInt16(&H8)
+        entryLength = RUInt16(&HA)
 
 
         ReDim paramDef(numEntries - 1)
@@ -272,11 +313,11 @@ Public Class frmParamEdit
         dgvParams.Columns.Add("ID (Dec)", "ID (Dec)")
 
         For i = 0 To numEntries - 1
-            paramType = StrFromBytes(startOffset + &H40 + (entryLength * i))
-            paramMin = SingleFromFour(startOffset + &H54 + (entryLength * i))
-            paramMax = SingleFromFour(startOffset + &H58 + (entryLength * i))
-            paramSize = UIntFromFour(startOffset + &H64 + (entryLength * i))
-            paramName = StrFromBytes(startOffset + &H8C + (entryLength * i))
+            paramType = RAscStr(startOffset + &H40 + (entryLength * i))
+            paramMin = RSingle(startOffset + &H54 + (entryLength * i))
+            paramMax = RSingle(startOffset + &H58 + (entryLength * i))
+            paramSize = RUInt32(startOffset + &H64 + (entryLength * i))
+            paramName = RAscStr(startOffset + &H8C + (entryLength * i))
 
             If paramName.Contains(":") Then
                 paramType = "bool"
@@ -293,23 +334,26 @@ Public Class frmParamEdit
             paramDef(i).paramMin = paramMin
             paramDef(i).paramMax = paramMax
 
-            
+
 
             dgvParams.Columns.Add(paramDef(i).paramName, paramDef(i).paramName)
         Next
+        fs.Close()
 
-        bytes = File.ReadAllBytes(txtParam.Text)
-        numEntries = UIntFromTwo(&HA)
+
+        fs = New IO.FileStream(txtParam.Text, IO.FileMode.Open)
+
+        numEntries = RUInt16(&HA)
 
         For i = 0 To numEntries - 1
             Dim row(paramDef.Count + 2) As String
 
-            row(0) = Hex(UIntFromFour(&H30 + (&HC * i)))
-            row(1) = UIntFromFour(&H30 + (&HC * i))
+            row(0) = Hex(RUInt32(&H30 + (&HC * i)))
+            row(1) = RUInt32(&H30 + (&HC * i))
 
 
 
-            offset = UIntFromFour(&H34 + (&HC * i))
+            offset = RUInt32(&H34 + (&HC * i))
 
             paramDefOffset = 0
 
@@ -319,7 +363,7 @@ Public Class frmParamEdit
             For j = 0 To paramDef.Length - 1
                 Select Case paramDef(j).paramType
                     Case "bool"
-                        row(j + 2) = ((CByte(bytes(offset + paramDefOffset)) and bitarray) > 0)
+                        row(j + 2) = ((RUInt8(offset + paramDefOffset) And bitarray) > 0)
                         bitarray = bitarray * 2
                         If bitarray = 256 Then
                             bitarray = 1
@@ -342,19 +386,19 @@ Public Class frmParamEdit
 
                         paramDefOffset += pad
                     Case "f32"
-                        row(j + 2) = SingleFromFour(offset + paramDefOffset)
+                        row(j + 2) = RSingle(offset + paramDefOffset)
                     Case "s8"
-                        row(j + 2) = CSByte(bytes(offset + paramDefOffset))
+                        row(j + 2) = RInt8(offset + paramDefOffset)
                     Case "s16"
-                        row(j + 2) = SIntFromTwo(offset + paramDefOffset)
+                        row(j + 2) = RInt16(offset + paramDefOffset)
                     Case "s32"
-                        row(j + 2) = SIntFromFour(offset + paramDefOffset)
+                        row(j + 2) = RInt32(offset + paramDefOffset)
                     Case "u8"
-                        row(j + 2) = CByte(bytes(offset + paramDefOffset))
+                        row(j + 2) = RUInt8(offset + paramDefOffset)
                     Case "u16"
-                        row(j + 2) = UIntFromTwo(offset + paramDefOffset)
+                        row(j + 2) = RUInt16(offset + paramDefOffset)
                     Case "u32"
-                        row(j + 2) = UIntFromFour(offset + paramDefOffset)
+                        row(j + 2) = RUInt32(offset + paramDefOffset)
                 End Select
 
                 paramDefOffset += paramDef(j).paramSize
@@ -364,27 +408,29 @@ Public Class frmParamEdit
 
             dgvParams.Rows.Add(row)
 
-            offset = UIntFromFour(&H38 + (&HC * i))
+            offset = RUInt32(&H38 + (&HC * i))
 
         Next
+        fs.Close()
+
         dgvParams.Columns(1).Frozen = True
         dgvParams.AutoResizeColumns()
 
-        For each column as DataGridViewColumn In dgvparams.columns
+        For Each column As DataGridViewColumn In dgvParams.Columns
             column.SortMode = DataGridViewColumnSortMode.NotSortable
         Next
 
 
     End Sub
     Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
-        bytes = File.ReadAllBytes(txtParam.Text)
+        fs = New IO.FileStream(txtParam.Text, IO.FileMode.Open)
         Dim paramDefOffset As UInteger
         Dim offset As UInteger
 
         For i = 0 To dgvParams.Rows.Count - 2
             paramDefOffset = 0
 
-            offset = UIntFromFour(&H34 + (&HC * i))
+            offset = RUInt32(&H34 + (&HC * i))
 
             Dim bitfield = 0
             Dim bitval = 0
@@ -398,7 +444,7 @@ Public Class frmParamEdit
                         End If
                         bitfield += 1
                         If bitfield = 8 Then
-                            bytes(offset + paramDefOffset) = Convert.ToByte(bitval)
+                            WUInt8(offset + paramDefOffset, Convert.ToByte(bitval))
                             paramDefOffset += 1
                             bitval = 0
                             bitfield = 0
@@ -413,33 +459,33 @@ Public Class frmParamEdit
 
                         If bitfield > 0 Then
                             bitfield = 0
-                            bytes(offset + paramDefOffset) = Convert.ToByte(bitval)
+                            WUInt8(offset + paramDefOffset, Convert.ToByte(bitval))
                             paramDefOffset += 1
                         End If
 
                         For k = 0 To val(pad) - 1
-                            bytes(offset + paramDefOffset) = Convert.ToByte(0)
+                            WUInt8(offset + paramDefOffset, 0)
                             paramDefOffset += 1
                         Next
                     Case "f32"
-                        InsBytes(offset + paramDefOffset, FloatToFourByte(dgvParams.Rows(i).Cells(j + 2).FormattedValue))
+                        WSingle(offset + paramDefOffset, dgvParams.Rows(i).Cells(j + 2).FormattedValue)
                     Case "s8"
-                        bytes(offset + paramDefOffset) = Convert.ToSByte(dgvParams.Rows(i).Cells(j + 2).FormattedValue)
+                        WInt8(offset + paramDefOffset, dgvParams.Rows(i).Cells(j + 2).FormattedValue)
                     Case "s16"
-                        InsBytes(offset + paramDefOffset, Int16ToTwoByte(dgvParams.Rows(i).Cells(j + 2).FormattedValue))
+                        WInt16(offset + paramDefOffset, dgvParams.Rows(i).Cells(j + 2).FormattedValue)
                     Case "s32"
-                        InsBytes(offset + paramDefOffset, Int32ToFourByte(dgvParams.Rows(i).Cells(j + 2).FormattedValue))
+                        WInt32(offset + paramDefOffset, dgvParams.Rows(i).Cells(j + 2).FormattedValue)
                     Case "u8"
-                        bytes(offset + paramDefOffset) = Convert.ToByte(dgvParams.Rows(i).Cells(j + 2).FormattedValue)
+                        WUInt8(offset + paramDefOffset, dgvParams.Rows(i).Cells(j + 2).FormattedValue)
                     Case "u16"
-                        InsBytes(offset + paramDefOffset, UInt16TotwoByte(dgvParams.Rows(i).Cells(j + 2).FormattedValue))
+                        WUInt16(offset + paramDefOffset, dgvParams.Rows(i).Cells(j + 2).FormattedValue)
                     Case "u32"
-                        InsBytes(offset + paramDefOffset, UInt32ToFourByte(dgvParams.Rows(i).Cells(j + 2).FormattedValue))
+                        WUInt32(offset + paramDefOffset, dgvParams.Rows(i).Cells(j + 2).FormattedValue)
                 End Select
                 paramDefOffset += paramDef(j).paramSize
             Next
         Next
-        File.WriteAllBytes(txtParam.Text, bytes)
+        fs.Close()
         MsgBox("Save complete.")
     End Sub
 
