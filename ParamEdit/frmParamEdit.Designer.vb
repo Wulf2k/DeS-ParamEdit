2f1c222574b96fe8c405236f6120a080f3adac5b
Wulf2k <wulf2k@gmail.com>
2017-06-28 20:44:37 -0500

    Updated label for compiled EXE release.

--- ParamEdit/frmParamEdit.Designer.vb
+++ ParamEdit/frmParamEdit.Designer.vb
@@ -146,7 +146,7 @@ Partial Class frmParamEdit
         Me.lblVer.Name = "lblVer"
         Me.lblVer.Size = New System.Drawing.Size(76, 13)
         Me.lblVer.TabIndex = 77
-        Me.lblVer.Text = "2017.05.11.15"
+        Me.lblVer.Text = "2017.06.28.20"
         '
         'frmParamEdit
         '
